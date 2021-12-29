e1069858c01008c60b98b56719f5dda20946d05d
zh <zhihuei94@gmail.com>
2021-12-29 11:22:49 +0800

    remove some redundant

--- reel-gui/src/utils/helper.js
+++ reel-gui/src/utils/helper.js
@@ -3,7 +3,6 @@ class Helper {
   static parseTimecodeToSeconds(timecode, standard) {
     const arr = timecode.split(':');
 
-    // frame to second arr[3] * 0.04 (pal)
     let s = 0;
     switch (standard) {
       case 'PAL':
@@ -17,12 +16,9 @@ class Helper {
     }
     // hour * 3600 + minute * 60 + seconds
     return Number(arr[0]) * 3600 + Number(arr[1]) * 60 + Number(arr[2]) + s;
-
-
   }
 
   static convertSecondsToHMS(duration, standard) {
-    console.log(duration)
     const h = Math.floor(duration / 3600);
     const m = Math.floor(duration % 3600 / 60);
     const s = Math.floor(duration % 3600 % 60);
